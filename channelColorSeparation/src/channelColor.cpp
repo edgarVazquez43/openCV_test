762c8cdde16871e1e607bde206a9dca9436f8917
Edgar Vazquez <edgar_vazquez43@hotmail.com>
2019-02-26 20:46:37 -0600

    Split image by their color channels

--- channelColorSeparation/src/channelColor.cpp
+++ channelColorSeparation/src/channelColor.cpp
@@ -8,17 +8,32 @@ int main(int argc, char** argv )
     std::cout << "Error un argumentos" << std::endl;
     return -1;
   }
+  image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
 
   cv::Mat image;
-  cv::Mat rgb[3];
-  image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
-  cv::split(image, rgb);
+  cv::Vec3b px;
+   
+  std::cout << "origin[rows]: " << image.rows << std::endl;
+  std::cout << "origin[cols]: " << image.cols << std::endl;
   
-  
+  cv::Mat r = cv::Mat::zeros(image.rows, image.cols, CV_8UC3);
+  cv::Mat g = cv::Mat::zeros(image.rows, image.cols, CV_8UC3);
+  cv::Mat b = cv::Mat::zeros(image.rows, image.cols, CV_8UC3);
+   
+  for(int i = 0; i < image.rows; i++)
+    for(int j = 0; j < image.cols; j++)
+    {
+      px = image.at<cv::Vec3b>(i, j);
+      r.at<cv::Vec3b>(i, j).val[0] = image.at<cv::Vec3b>(i, j).val[0];
+      g.at<cv::Vec3b>(i, j).val[1] = image.at<cv::Vec3b>(i, j).val[1];
+      b.at<cv::Vec3b>(i, j).val[2] = image.at<cv::Vec3b>(i, j).val[2];
+    }
+
   cv::imshow("original", image);
-  cv::imshow("Blue window", rgb[0]);
-  cv::imshow("Green window", rgb[1]);
-  cv::imshow("Red window", rgb[2]);
+  cv::imshow("Blue window",  b);
+  cv::imshow("Green window", g);
+  cv::imshow("Red window",   r);
 
   cv::waitKey(0);
   return 0;
